a2f268bcf7955f076577e28ba4f98017f82340c4
VitaliyLF <97784759+Vetosy@users.noreply.github.com>
2025-01-02 22:12:39 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -1,1 +1,126 @@
-(()=>{"use strict";new class{selectors={root:"[data-js-header]",overlay:"[data-js-header-overlay]",burgerButton:"[data-js-burger-button]"};stateClasses={isActive:"is-active",isLock:"is-lock"};constructor(){this.rootElement=document.querySelector(this.selectors.root),this.overlayElement=this.rootElement.querySelector(this.selectors.overlay),this.burgerButtonElement=this.rootElement.querySelector(this.selectors.burgerButton),this.bindEvents()}onBurgerButtonClick=()=>{this.burgerButtonElement.classList.toggle(this.stateClasses.isActive),this.overlayElement.classList.toggle(this.stateClasses.isActive),document.documentElement.classList.toggle(this.stateClasses.isLock)};bindEvents(){this.burgerButtonElement.addEventListener("click",this.onBurgerButtonClick)}}})();
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/_components.js":
+/*!*******************************!*\
+  !*** ./src/js/_components.js ***!
+  \*******************************/
+/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _components_header_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./components/header.js */ "./src/js/components/header.js");
+
+
+/***/ }),
+
+/***/ "./src/js/components/header.js":
+/*!*************************************!*\
+  !*** ./src/js/components/header.js ***!
+  \*************************************/
+/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+class Header {
+  selectors = {
+    root: '[data-js-header]',
+    overlay: '[data-js-header-overlay]',
+    burgerButton: '[data-js-burger-button]'
+  };
+  stateClasses = {
+    isActive: 'is-active',
+    isLock: 'is-lock'
+  };
+  constructor() {
+    this.rootElement = document.querySelector(this.selectors.root);
+    this.overlayElement = this.rootElement.querySelector(this.selectors.overlay);
+    this.burgerButtonElement = this.rootElement.querySelector(this.selectors.burgerButton);
+    this.bindEvents();
+  }
+  onBurgerButtonClick = () => {
+    this.burgerButtonElement.classList.toggle(this.stateClasses.isActive);
+    this.overlayElement.classList.toggle(this.stateClasses.isActive);
+    document.documentElement.classList.toggle(this.stateClasses.isLock);
+  };
+  bindEvents() {
+    this.burgerButtonElement.addEventListener('click', this.onBurgerButtonClick);
+  }
+}
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Header);
+new Header();
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry needs to be wrapped in an IIFE because it needs to be isolated against other modules in the chunk.
+(() => {
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _components_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./_components.js */ "./src/js/_components.js");
+
+})();
+
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
